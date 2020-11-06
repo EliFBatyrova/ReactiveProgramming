9b7fb22be225f24b5b19935016554b9a7da15316
underq <maks.myalicin@gmail.com>
2020-11-06 18:33:39 +0300

    added dependencies

---
+++ ReactiveProgramming/ReactiveProgramming/ReactiveProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactiveProgrammingApp.swift
+//  ReactiveProgramming
+//
+//  Created by underq  on 06.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ReactiveProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
